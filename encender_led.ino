f4ccc9bfc49e10504bad0f3a869ac3c94d53488a
davgonzalvo <57009847+davgonzalvo@users.noreply.github.com>
2020-05-14 13:21:36 +0200

    Add files via upload

---
+++ encender_led.ino
@@ -0,0 +1,17 @@
+
+// encender led
+
+int led = 2;
+void setup() {
+ pinMode(led, OUTPUT);
+ digitalWrite(led, LOW);
+
+}
+
+void loop() {
+  digitalWrite(led, HIGH);
+  delay(1000);
+  digitalWrite(led, LOW);
+  delay(1000);
+
+}
